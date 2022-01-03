3a7390e69982c611a3902cc07b1e95469f81fe46
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2022-01-03 12:28:38 -0500

    Update eval.py

--- eval.py
+++ eval.py
@@ -368,6 +368,29 @@ def load_model(model_type):
     std=[0.229, 0.224, 0.225])
     ])
     return resnet
+  if model_type=="v_resnet_60" or  model_type=="v_resnet_30" or  model_type=="v_resnet_0" or  model_type=="v_resnet_90":
+    resnet=models.resnet50(pretrained=False)
+    epoch_num=model_type.split('_')[2]
+    if model_type=="v_resnet_90":
+      checkpoint = torch.load(f'/content/gdrive/MyDrive/model_checkpoints/model_best.pth.tar',map_location=torch.device('cpu') )
+    else:
+      checkpoint = torch.load(f'/content/gdrive/MyDrive/model_checkpoints/model_epoch{epoch_num}.pth.tar',map_location=torch.device('cpu') )
+    state_dict=checkpoint['state_dict']
+    for k in list(state_dict.keys()):
+        if k.startswith('module.') :
+
+            state_dict[k[len('module.'):]] = state_dict[k]
+        del state_dict[k]
+    resnet.load_state_dict(state_dict)
+    preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+    mean=[0.485, 0.456, 0.406],
+    std=[0.229, 0.224, 0.225])
+    ])
+    return resnet
 
 
 # net = models.densenet121(pretrained=True)
